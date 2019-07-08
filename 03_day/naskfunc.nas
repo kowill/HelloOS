838127c58bfdd7bb352509ea2319944492efbab9
kowill <mob.koya@gmail.com>
2019-07-08 14:02:10 +0900

    day3 complete

---
+++ 03_day/naskfunc.nas
@@ -0,0 +1,12 @@
+; naskfunc
+
+[FORMAT "WCOFF"]
+[BITS 32]
+
+[FILE "naskfunc.nas"]
+    GLOBAL  _io_hlt
+
+[SECTION .text]
+_io_hlt:        ; void io_hlt(void);
+    HLT
+    RET
--- build.bat
+++ build.bat
@@ -2,17 +2,17 @@ setlocal
 
 del /Q boot.vfd
 SET day=0%1
+SET targetPath=%day:~-2%_day
 
 REM ipl build
-SET iplPath=%day:~-2%_day\ipl.nas
-bin\tolset\z_tools\nask.exe %iplPath% tmp\ipl.bin
+bin\tolset\z_tools\nask.exe %targetPath%\ipl.nas tmp\ipl.bin
 
 REM asmhead build
-SET asmheadPath=%day:~-2%_day\asmhead.nas
+SET asmheadPath=%targetPath%\asmhead.nas
 bin\tolset\z_tools\nask.exe %asmheadPath% tmp\asmhead.bin tmp\asmhead.lst
 
 REM bootpack build
-SET bootpackPath=%day:~-2%_day\bootpack.c
+SET bootpackPath=%targetPath%\bootpack.c
 bin\tolset\z_tools\cc1.exe -I bin\tolset\z_tools\haribote\ -Os -Wall -quiet -o tmp\bootpack.gas %bootpackPath%
 
 REM gas 2 nask
@@ -20,9 +20,10 @@ bin\tolset\z_tools\gas2nask.exe -a tmp\bootpack.gas tmp\bootpack.nas
 
 REM nas 2 obj
 bin\tolset\z_tools\nask.exe tmp\bootpack.nas tmp\bootpack.obj
+bin\tolset\z_tools\nask.exe %targetPath%\naskfunc.nas tmp\naskfunc.obj
 
 REM obj 2 bim
-bin\tolset\z_tools\obj2bim.exe @bin\tolset\z_tools\haribote\haribote.rul out:tmp\bootpack.bim stack:3136k map:tmp\bootpack.map tmp\bootpack.obj
+bin\tolset\z_tools\obj2bim.exe @bin\tolset\z_tools\haribote\haribote.rul out:tmp\bootpack.bim stack:3136k map:tmp\bootpack.map tmp\bootpack.obj tmp\naskfunc.obj
 
 REM bim 2 hrb
 bin\tolset\z_tools\bim2hrb.exe tmp\bootpack.bim tmp\bootpack.hrb 0
