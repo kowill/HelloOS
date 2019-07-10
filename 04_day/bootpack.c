b7f0150ec697eea465e09178597bdc0aeffe4210
kowill <mob.koya@gmail.com>
2019-07-10 14:04:39 +0900

    day4 complete

--- 04_day/bootpack.c
+++ 04_day/bootpack.c
@@ -1,3 +1,20 @@
+#define COL8_000000 0
+#define COL8_FF0000 1
+#define COL8_00FF00 2
+#define COL8_FFFF00 3
+#define COL8_0000FF 4
+#define COL8_FF00FF 5
+#define COL8_00FFFF 6
+#define COL8_FFFFFF 7
+#define COL8_C6C6C6 8
+#define COL8_840000 9
+#define COL8_008400 10
+#define COL8_848400 11
+#define COL8_000084 12
+#define COL8_840084 13
+#define COL8_008484 14
+#define COL8_848484 15
+
 void io_hlt(void);
 void io_cli(void);
 void io_out8(int port, int data);
@@ -7,22 +24,61 @@ void io_store_eflags(int eflags);
 void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
 
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+
+void show_boxes();
+void show_home();
+
 void HariMain(void)
 {
-    int i;
-
     init_palette();
 
-    for (i = 0xa0000; i <= 0xaffff; i++)
-    {
-        // write_mem8(i, i & 0x0f);
-        *((char *)i) = i & 0x0f;
-    }
+    show_home();
 
     for (;;)
         io_hlt();
 }
 
+void show_home()
+{
+    char *vram = (char *)0xa0000;
+    int xsize = 320;
+    int ysize = 200;
+
+    boxfill8(vram, xsize, COL8_008484, 0, 0, xsize - 1, ysize - 29);
+    boxfill8(vram, xsize, COL8_C6C6C6, 0, ysize - 28, xsize - 1, ysize - 28);
+    boxfill8(vram, xsize, COL8_FFFFFF, 0, ysize - 27, xsize - 1, ysize - 27);
+    boxfill8(vram, xsize, COL8_C6C6C6, 0, ysize - 26, xsize - 1, ysize - 1);
+
+    boxfill8(vram, xsize, COL8_FFFFFF, 3, ysize - 24, 59, ysize - 24);
+    boxfill8(vram, xsize, COL8_FFFFFF, 2, ysize - 24, 2, ysize - 4);
+    boxfill8(vram, xsize, COL8_848484, 3, ysize - 4, 59, ysize - 4);
+    boxfill8(vram, xsize, COL8_848484, 59, ysize - 23, 59, ysize - 5);
+    boxfill8(vram, xsize, COL8_000000, 2, ysize - 3, 59, ysize - 3);
+    boxfill8(vram, xsize, COL8_000000, 60, ysize - 24, 60, ysize - 3);
+
+    boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize - 4, ysize - 24);
+    boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize - 4);
+    boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize - 3, xsize - 4, ysize - 3);
+    boxfill8(vram, xsize, COL8_FFFFFF, xsize - 3, ysize - 24, xsize - 3, ysize - 3);
+}
+
+void show_boxes()
+{
+    char *p = (char *)0xa0000;
+
+    boxfill8(p, 320, COL8_FF0000, 20, 20, 120, 120);
+    boxfill8(p, 320, COL8_00FF00, 70, 50, 170, 150);
+    boxfill8(p, 320, COL8_0000FF, 120, 80, 220, 180);
+}
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1)
+{
+    int x, y;
+    for (y = y0; y <= y1; y++)
+        for (x = x0; x <= x1; x++)
+            vram[y * xsize + x] = c;
+    return;
+}
 void init_palette(void)
 {
     static unsigned char table_rgb[16 * 3] = {
@@ -63,4 +119,3 @@ void set_palette(int start, int end, unsigned char *rgb)
     io_store_eflags(eflags); /* 割り込み許可フラグ */
     return;
 }
-
