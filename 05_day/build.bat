5a4ae0db3bf8bf518611739e0fc32fb8d8a80669
kowill <mob.koya@gmail.com>
2019-07-11 17:38:41 +0900

    backup

---
+++ 05_day/build.bat
@@ -0,0 +1,40 @@
+@echo off
+setlocal
+
+del /Q boot.vfd
+SET day=0%1
+SET targetPath=%day:~-2%_day
+
+REM ipl build
+bin\tolset\z_tools\nask.exe %targetPath%\ipl.nas tmp\ipl.bin
+
+REM asmhead build
+SET asmheadPath=%targetPath%\asmhead.nas
+bin\tolset\z_tools\nask.exe %asmheadPath% tmp\asmhead.bin tmp\asmhead.lst
+
+REM bootpack build
+SET bootpackPath=%targetPath%\bootpack.c
+bin\tolset\z_tools\cc1.exe -I bin\tolset\z_tools\haribote\ -Os -Wall -quiet -o tmp\bootpack.gas %bootpackPath%
+
+REM gas 2 nask
+bin\tolset\z_tools\gas2nask.exe -a tmp\bootpack.gas tmp\bootpack.nas
+
+REM nas 2 obj
+bin\tolset\z_tools\nask.exe tmp\bootpack.nas tmp\bootpack.obj
+bin\tolset\z_tools\nask.exe %targetPath%\naskfunc.nas tmp\naskfunc.obj tmp\naskfunc.lst
+
+REM font section
+bin\tolset\z_tools\makefont.exe %targetPath%\hankaku.txt tmp\hankaku.bin
+bin\tolset\z_tools\bin2obj.exe tmp\hankaku.bin tmp\hankaku.obj _hankaku
+
+REM obj 2 bim
+bin\tolset\z_tools\obj2bim.exe @bin\tolset\z_tools\haribote\haribote.rul out:tmp\bootpack.bim stack:3136k map:tmp\bootpack.map tmp\bootpack.obj tmp\naskfunc.obj tmp\hankaku.obj
+
+REM bim 2 hrb
+bin\tolset\z_tools\bim2hrb.exe tmp\bootpack.bim tmp\bootpack.hrb 0
+
+REM copy
+copy /B tmp\asmhead.bin+tmp\bootpack.hrb tmp\haribote.sys
+
+REM edimg
+bin\tolset\z_tools\edimg.exe @edimg_script.txt
