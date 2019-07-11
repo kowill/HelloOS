068806f025b7fdc36cc42c64e2574dbe3f274c06
kowill <mob.koya@gmail.com>
2019-07-11 15:27:46 +0900

    day5 complete

--- 05_day/bootpack.c
+++ 05_day/bootpack.c
@@ -24,6 +24,20 @@ struct BOOTINFO
     char *vram;
 };
 
+struct SEGMENT_DESCRIPTOR
+{
+    short limit_low, base_low;
+    char base_mid, access_right;
+    char limit_high, base_high;
+};
+
+struct GATE_DESCRIPTOR
+{
+    short offset_low, selector;
+    char dw_count, access_right;
+    short offset_high;
+};
+
 void io_hlt(void);
 void io_cli(void);
 void io_out8(int port, int data);
@@ -42,11 +56,18 @@ void show_boxes();
 void init_screen(char *vram, int xsize, int ysize);
 void init_mouse_coursor8(char *mouse, char bc);
 
+void init_gdtidt(void);
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
+void load_gdtr(int limit, int addr);
+void load_idtr(int limit, int addr);
+
 void HariMain(void)
 {
     struct BOOTINFO *binfo = (struct BOOTINFO *)0x0ff0;
     char s[20], mousecursor[256];
 
+    init_gdtidt();
     init_palette();
     init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
     init_mouse_coursor8(mousecursor, COL8_008484);
@@ -222,5 +243,52 @@ void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0, int py
     for (y = 0; y < pysize; y++)
         for (x = 0; x < pxsize; x++)
             vram[(py0 + y) * vxsize + (px0 + x)] = buf[y * bxsize + x];
+    return;
+}
+
+void init_gdtidt(void)
+{
+    struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *)0x00270000;
+    struct GATE_DESCRIPTOR *idt = (struct GATE_DESCRIPTOR *)0x0026f800;
+    int i;
+
+    for (i = 0; i < 8192; i++)
+        set_segmdesc(gdt + i, 0, 0, 0);
+
+    set_segmdesc(gdt + 1, 0xffffffff, 0x00000000, 0x4092);
+    set_segmdesc(gdt + 2, 0x0007ffff, 0x00280000, 0x409a);
+    load_gdtr(0xffff, 0x00270000);
+
+    for (i = 0; i < 256; i++)
+        set_gatedesc(idt + i, 0, 0, 0);
+
+    load_idtr(0x7ff, 0x0026f800);
+
+    return;
+}
+
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar)
+{
+    if (limit > 0xfffff)
+    {
+        ar |= 0x8000;
+        limit /= 0x1000;
+    }
+    sd->limit_low = limit & 0xffff;
+    sd->base_low = base & 0xffff;
+    sd->base_mid = (base >> 16) & 0xff;
+    sd->access_right = ar & 0xff;
+    sd->limit_high = ((limit >> 16) & 0x0f) | ((ar >> 8) & 0xf0);
+    sd->base_high = (base >> 24) & 0xff;
+    return;
+}
+
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar)
+{
+    gd->offset_low = offset & 0xffff;
+    gd->selector = selector;
+    gd->dw_count = (ar >> 8) & 0xff;
+    gd->access_right = ar & 0xff;
+    gd->offset_high = (offset >> 16) & 0xffff;
     return;
 }
