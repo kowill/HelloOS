38152f79e3f0ed8b50c43b3fa631197c9f54bb77
kowill <mob.koya@gmail.com>
2019-07-13 17:59:25 +0900

    day7 complete

--- 07_day/int.c
+++ 07_day/int.c
@@ -1,6 +1,6 @@
 #include "bootpack.h"
 
-struct FIFO8 keyfifo;
+struct FIFO8 keyfifo, mousefifo;
 
 void init_pic(void)
 {
@@ -36,9 +36,10 @@ void inthandler21(int *esp)
 void inthandler2c(int *esp)
 {
     /* from mouse */
-    struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
-    boxfill8(binfo->vram, binfo->scrnx, COL8_000000, 0, 0, 32 * 8 - 1, 15);
-    putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, "INT 2C (IRQ-12) : PS/2 mouse");
-    for (;;)
-        io_hlt();
+    unsigned char data;
+    io_out8(PIC1_OCW2, 0x64); // IRQ-12受付完了
+    io_out8(PIC0_OCW2, 0x62); // IRQ-02受付完了
+    data = io_in8(PORT_KEYDAT);
+    fifo8_put(&mousefifo, data);
+    return;
 }
