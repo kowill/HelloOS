27fd499d4003d9ff02a366a211f4567f1b8c5b49
kowill <mob.koya@gmail.com>
2019-07-13 18:38:18 +0900

    8-3

--- 08_day/bootpack.c
+++ 08_day/bootpack.c
@@ -2,15 +2,16 @@
 #include "bootpack.h"
 
 extern struct FIFO8 keyfifo, mousefifo;
-void enable_mouse(void);
+void enable_mouse(struct MOUSE_DEC *mdec);
 void init_keyboard(void);
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char data);
 
 void HariMain(void)
 {
     struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
+    struct MOUSE_DEC mdec;
     char s[20], mousecursor[256], keybuf[32], mousebuf[128];
     int i;
-    unsigned char mouse_dbuf[8], mouse_phase;
 
     init_gdtidt();
     init_pic();
@@ -29,9 +30,7 @@ void HariMain(void)
     putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, "HELLO, WORLD!!");
 
     init_keyboard();
-    enable_mouse();
-
-    mouse_phase = 0;
+    enable_mouse(&mdec);
 
     for (;;)
     {
@@ -54,28 +53,16 @@ void HariMain(void)
             {
                 i = fifo8_get(&mousefifo);
                 io_sti();
-                if (mouse_phase == 0)
+                if (mouse_decode(&mdec, i) != 0)
                 {
-                    if (i == 0xfa)
-                        mouse_phase = 1;
-                }
-                else if (mouse_phase == 1)
-                {
-                    mouse_dbuf[0] = i;
-                    mouse_phase = 2;
-                }
-                else if (mouse_phase == 2)
-                {
-                    mouse_dbuf[1] = i;
-                    mouse_phase = 3;
-                }
-                else if (mouse_phase == 3)
-                {
-                    mouse_dbuf[2] = i;
-                    mouse_phase = 1;
-
-                    sprintf(s, "%02X %02X %02X", mouse_dbuf[0], mouse_dbuf[1], mouse_dbuf[2]);
-                    boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 8 * 8 - 1, 31);
+                    sprintf(s, "[lcr %4d %4d]", mdec.x, mdec.y);
+                    if ((mdec.btn & 0x01) != 0)
+                        s[1] = 'L';
+                    if ((mdec.btn & 0x02) != 0)
+                        s[3] = 'R';
+                    if ((mdec.btn & 0x04) != 0)
+                        s[2] = 'C';
+                    boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 15 * 8 - 1, 31);
                     putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
                 }
             }
@@ -104,11 +91,53 @@ void init_keyboard(void)
     return;
 }
 
-void enable_mouse(void)
+void enable_mouse(struct MOUSE_DEC *mdec)
 {
+    // enable
     wait_KBC_sendready();
     io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
     wait_KBC_sendready();
     io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
+    mdec->phase = 0;
     return;
+}
+
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char data)
+{
+    if (mdec->phase == 0)
+    {
+        if (data == 0xfa)
+            mdec->phase = 1;
+        return 0;
+    }
+    if (mdec->phase == 1)
+    {
+        if ((data & 0xc8) == 0x08)
+        {
+            mdec->buf[0] = data;
+            mdec->phase = 2;
+        }
+        return 0;
+    }
+    if (mdec->phase == 2)
+    {
+        mdec->buf[1] = data;
+        mdec->phase = 3;
+        return 0;
+    }
+    if (mdec->phase == 3)
+    {
+        mdec->buf[2] = data;
+        mdec->phase = 1;
+        mdec->btn = mdec->buf[0] & 0x07;
+        mdec->x = mdec->buf[1];
+        mdec->y = mdec->buf[2];
+        if ((mdec->buf[0] & 0x10) != 0)
+            mdec->x |= 0xffffff00;
+        if ((mdec->buf[0] & 0x20) != 0)
+            mdec->y |= 0xffffff00;
+        mdec->y = -mdec->y;
+        return 1;
+    }
+    return -1;
 }
--- 08_day/bootpack.h
+++ 08_day/bootpack.h
@@ -7,6 +7,12 @@
 #define KEYCMD_SENDTO_MOUSE 0xd4
 #define MOUSECMD_ENABLE 0xf4
 
+struct MOUSE_DEC
+{
+    unsigned char buf[3], phase;
+    int x, y, btn;
+};
+
 /* asmhead.nas */
 #define ADR_BOOTINFO 0x0000ff0
 
