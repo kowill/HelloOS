4fd9bb3a90c7757cd2dc665ea0623cfdc06102c5
kowill <mob.koya@gmail.com>
2019-07-13 18:07:51 +0900

    8-1

--- 08_day/bootpack.c
+++ 08_day/bootpack.c
@@ -10,6 +10,7 @@ void HariMain(void)
     struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
     char s[20], mousecursor[256], keybuf[32], mousebuf[128];
     int i;
+    unsigned char mouse_dbuf[8], mouse_phase;
 
     init_gdtidt();
     init_pic();
@@ -29,6 +30,8 @@ void HariMain(void)
 
     init_keyboard();
     enable_mouse();
+
+    mouse_phase = 0;
 
     for (;;)
     {
@@ -51,9 +54,30 @@ void HariMain(void)
             {
                 i = fifo8_get(&mousefifo);
                 io_sti();
-                sprintf(s, "%02X", i);
-                boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 47, 31);
-                putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+                if (mouse_phase == 0)
+                {
+                    if (i == 0xfa)
+                        mouse_phase = 1;
+                }
+                else if (mouse_phase == 1)
+                {
+                    mouse_dbuf[0] = i;
+                    mouse_phase = 2;
+                }
+                else if (mouse_phase == 2)
+                {
+                    mouse_dbuf[1] = i;
+                    mouse_phase = 3;
+                }
+                else if (mouse_phase == 3)
+                {
+                    mouse_dbuf[2] = i;
+                    mouse_phase = 1;
+
+                    sprintf(s, "%02X %02X %02X", mouse_dbuf[0], mouse_dbuf[1], mouse_dbuf[2]);
+                    boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 8 * 8 - 1, 31);
+                    putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+                }
             }
         }
     }
