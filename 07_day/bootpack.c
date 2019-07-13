9d6386af6767da7f2499643386f33f7b82c33d04
kowill <mob.koya@gmail.com>
2019-07-13 16:54:06 +0900

    7-6 mouse!!

--- 07_day/bootpack.c
+++ 07_day/bootpack.c
@@ -2,6 +2,8 @@
 #include "bootpack.h"
 
 extern struct FIFO8 keyfifo;
+void enable_mouse(void);
+void init_keyboard(void);
 
 void HariMain(void)
 {
@@ -13,16 +15,19 @@ void HariMain(void)
     init_pic();
     io_sti();
 
-    init_palette();
-    init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
-    init_mouse_coursor8(mousecursor, COL8_008484);
-
-    putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, "HELLO, WORLD!!");
-
+    fifo8_init(&keyfifo, sizeof(keybuf), keybuf);
     io_out8(PIC0_IMR, 0xf9);
     io_out8(PIC1_IMR, 0xef);
 
-    fifo8_init(&keyfifo, sizeof(keybuf), keybuf);
+    init_palette();
+    init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
+    init_mouse_coursor8(mousecursor, COL8_008484);
+    putblock8_8(binfo->vram, binfo->scrnx, 16, 16, (binfo->scrnx - 16) / 2, (binfo->scrny - 28 - 16) / 2, mousecursor, 16);
+
+    putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, "HELLO, WORLD!!");
+
+    init_keyboard();
+    enable_mouse();
 
     for (;;)
     {
@@ -40,4 +45,34 @@ void HariMain(void)
             putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
         }
     }
+}
+
+void wait_KBC_sendready(void)
+{
+    /* wait until keybord controller is ready to send data */
+    for (;;)
+    {
+        if ((io_in8(PORT_KEYSTA) & KEYSTA_SEND_NORTEADY) == 0)
+        {
+            break;
+        }
+    }
+}
+
+void init_keyboard(void)
+{
+    wait_KBC_sendready();
+    io_out8(PORT_KEYCMD, KEYCMD_WRITE_MODE);
+    wait_KBC_sendready();
+    io_out8(PORT_KEYDAT, KBC_MODE);
+    return;
+}
+
+void enable_mouse(void)
+{
+    wait_KBC_sendready();
+    io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
+    wait_KBC_sendready();
+    io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
+    return;
 }
--- 07_day/bootpack.h
+++ 07_day/bootpack.h
@@ -1,3 +1,12 @@
+#define PORT_KEYDAT 0x0060
+#define PORT_KEYSTA 0x0064
+#define PORT_KEYCMD 0x0064
+#define KEYSTA_SEND_NORTEADY 0x02
+#define KEYCMD_WRITE_MODE 0x60
+#define KBC_MODE 0x47
+#define KEYCMD_SENDTO_MOUSE 0xd4
+#define MOUSECMD_ENABLE 0xf4
+
 /* asmhead.nas */
 #define ADR_BOOTINFO 0x0000ff0
 
