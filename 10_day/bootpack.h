0812730d3421675e0f399561a45003ac7407f935
kowill <mob.koya@gmail.com>
2019-07-17 10:37:49 +0900

    add memory.c

--- 10_day/bootpack.h
+++ 10_day/bootpack.h
@@ -148,3 +148,10 @@ void enable_mouse(struct MOUSE_DEC *mdec);
 int mouse_decode(struct MOUSE_DEC *mdec, unsigned char data);
 
 extern struct FIFO8 mousefifo;
+
+/* memory.c */
+unsigned int memtest(unsigned int start, unsigned int end);
+void memman_init(struct MEMMAN *man);
+unsigned int memman_total(struct MEMMAN *man);
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
