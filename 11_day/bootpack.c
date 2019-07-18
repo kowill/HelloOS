85bc3bcfb5b7130892f8c8f2ef496322496bf008
kowill <mob.koya@gmail.com>
2019-07-19 02:05:35 +0900

    11-1

--- 11_day/bootpack.c
+++ 11_day/bootpack.c
@@ -91,10 +91,10 @@ void HariMain(void)
                         mx = 0;
                     if (my < 0)
                         my = 0;
-                    if (mx > binfo->scrnx - 16)
-                        mx = binfo->scrnx - 16;
-                    if (my > binfo->scrny - 16)
-                        my = binfo->scrny - 16;
+                    if (mx > binfo->scrnx - 1)
+                        mx = binfo->scrnx - 1;
+                    if (my > binfo->scrny - 1)
+                        my = binfo->scrny - 1;
                     sprintf(s, "(%3d, %3d)", mx, my);
                     boxfill8(buf_back, binfo->scrnx, COL8_008484, 0, 32, 79, 47);
                     putfonts8_asc(buf_back, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
