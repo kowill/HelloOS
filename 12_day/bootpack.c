74bb0c6fba6371e7b2e15329ae186551b7da0bfc
kowill <mob.koya@gmail.com>
2019-07-21 23:53:30 +0900

    12-4

--- 12_day/bootpack.c
+++ 12_day/bootpack.c
@@ -5,14 +5,15 @@ void HariMain(void)
 {
     struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
     struct MOUSE_DEC mdec;
-    char s[40], keybuf[32], mousebuf[128], timerbuf[8];
+    char s[40], keybuf[32], mousebuf[128], timerbuf[8], timerbuf2[8], timerbuf3[8];
     int i, mx, my;
     unsigned int memtotal;
     struct MEMMAN *memman = (struct MEMMAN *)MEMMAN_ADDR;
     struct SHTCTL *shtctl;
     struct SHEET *sht_back, *sht_mouse, *sht_win;
     unsigned char *buf_back, buf_mouse[256], *buf_win;
-    struct FIFO8 timerfifo;
+    struct FIFO8 timerfifo, timerfifo2, timerfifo3;
+    struct TIMER *timer, *timer2, *timer3;
 
     init_gdtidt();
     init_pic();
@@ -25,7 +26,19 @@ void HariMain(void)
     io_out8(PIC1_IMR, 0xef);
 
     fifo8_init(&timerfifo, 8, timerbuf);
-    settimer(1000, &timerfifo, 1);
+    timer = timer_alloc();
+    timer_init(timer, &timerfifo, 1);
+    timer_settime(timer, 1000);
+
+    fifo8_init(&timerfifo2, 8, timerbuf2);
+    timer2 = timer_alloc();
+    timer_init(timer2, &timerfifo2, 1);
+    timer_settime(timer2, 300);
+
+    fifo8_init(&timerfifo3, 8, timerbuf3);
+    timer3 = timer_alloc();
+    timer_init(timer3, &timerfifo3, 1);
+    timer_settime(timer3, 50);
 
     init_keyboard();
     enable_mouse(&mdec);
@@ -70,7 +83,7 @@ void HariMain(void)
         sheet_refresh(sht_win, 40, 28, 120, 44);
 
         io_cli();
-        if (fifo8_status(&keyfifo) + fifo8_status(&mousefifo) + fifo8_status(&timerfifo) == 0)
+        if (fifo8_status(&keyfifo) + fifo8_status(&mousefifo) + fifo8_status(&timerfifo) + fifo8_status(&timerfifo2) + fifo8_status(&timerfifo3) == 0)
         {
             io_sti();
         }
@@ -125,6 +138,30 @@ void HariMain(void)
                 io_sti();
                 putfonts8_asc(buf_back, binfo->scrnx, 0, 64, COL8_FFFFFF, "10[sec]");
                 sheet_refresh(sht_back, 0, 64, 56, 80);
+            }
+            else if (fifo8_status(&timerfifo2) != 0)
+            {
+                i = fifo8_get(&timerfifo2);
+                io_sti();
+                putfonts8_asc(buf_back, binfo->scrnx, 0, 80, COL8_FFFFFF, "3[sec]");
+                sheet_refresh(sht_back, 0, 80, 48, 96);
+            }
+            else if (fifo8_status(&timerfifo3) != 0)
+            {
+                i = fifo8_get(&timerfifo3);
+                io_sti();
+                if (i != 0)
+                {
+                    timer_init(timer3, &timerfifo3, 0);
+                    boxfill8(buf_back, binfo->scrnx, COL8_FFFFFF, 8, 96, 15, 111);
+                }
+                else
+                {
+                    timer_init(timer3, &timerfifo3, 1);
+                    boxfill8(buf_back, binfo->scrnx, COL8_008484, 8, 96, 15, 111);
+                }
+                timer_settime(timer3, 50);
+                sheet_refresh(sht_back, 8, 96, 16, 112);
             }
         }
     }
--- 12_day/bootpack.h
+++ 12_day/bootpack.h
@@ -193,16 +193,26 @@ void sheet_free(struct SHEET *sht);
 /* timer.c */
 #define PIT_CTRL 0x0043
 #define PIT_CNT0 0x0040
+#define MAX_TIMER 500
+#define TIMER_FLAGS_ALLOC 1
+#define TIMER_FLAGS_USING 2
+
+struct TIMER
+{
+    unsigned int timeout, flags;
+    struct FIFO8 *fifo;
+    unsigned char data;
+};
 
 struct TIMERCTL
 {
     unsigned int count;
-    unsigned int timeout;
-    struct FIFO8 *fifo;
-    unsigned char data;
+    struct TIMER timer[MAX_TIMER];
 };
 
 extern struct TIMERCTL timerctl;
 void init_pit(void);
 void inthandler20(int *esp);
-void settimer(unsigned int timeout, struct FIFO8 *fifo, unsigned char data);
+struct TIMER *timer_alloc(void);
+void timer_init(struct TIMER *timer, struct FIFO8 *fifo, unsigned char data);
+void timer_settime(struct TIMER *timer, unsigned int timeout);
