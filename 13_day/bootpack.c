fe42579250782288a39445d499f363e47acbd8c2
kowill <mob.koya@gmail.com>
2019-07-22 16:16:45 +0900

    13-2

--- 13_day/bootpack.c
+++ 13_day/bootpack.c
@@ -8,14 +8,14 @@ void HariMain(void)
 {
     struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
     struct MOUSE_DEC mdec;
-    char s[40], keybuf[32], mousebuf[128], timerbuf[8], timerbuf2[8], timerbuf3[8];
+    char s[40], keybuf[32], mousebuf[128], timerbuf[8];
     int i, mx, my;
     unsigned int memtotal;
     struct MEMMAN *memman = (struct MEMMAN *)MEMMAN_ADDR;
     struct SHTCTL *shtctl;
     struct SHEET *sht_back, *sht_mouse, *sht_win;
     unsigned char *buf_back, buf_mouse[256], *buf_win;
-    struct FIFO8 timerfifo, timerfifo2, timerfifo3;
+    struct FIFO8 timerfifo;
     struct TIMER *timer, *timer2, *timer3;
 
     init_gdtidt();
@@ -30,17 +30,15 @@ void HariMain(void)
 
     fifo8_init(&timerfifo, 8, timerbuf);
     timer = timer_alloc();
-    timer_init(timer, &timerfifo, 1);
+    timer_init(timer, &timerfifo, 10);
     timer_settime(timer, 1000);
 
-    fifo8_init(&timerfifo2, 8, timerbuf2);
     timer2 = timer_alloc();
-    timer_init(timer2, &timerfifo2, 1);
+    timer_init(timer2, &timerfifo, 3);
     timer_settime(timer2, 300);
 
-    fifo8_init(&timerfifo3, 8, timerbuf3);
     timer3 = timer_alloc();
-    timer_init(timer3, &timerfifo3, 1);
+    timer_init(timer3, &timerfifo, 1);
     timer_settime(timer3, 50);
 
     init_keyboard();
@@ -83,7 +81,7 @@ void HariMain(void)
         putfonts8_asc_sht(sht_win, 40, 28, COL8_000000, COL8_C6C6C6, s, 10);
 
         io_cli();
-        if (fifo8_status(&keyfifo) + fifo8_status(&mousefifo) + fifo8_status(&timerfifo) + fifo8_status(&timerfifo2) + fifo8_status(&timerfifo3) == 0)
+        if (fifo8_status(&keyfifo) + fifo8_status(&mousefifo) + fifo8_status(&timerfifo) == 0)
         {
             io_sti();
         }
@@ -130,30 +128,29 @@ void HariMain(void)
             {
                 i = fifo8_get(&timerfifo);
                 io_sti();
-                putfonts8_asc_sht(sht_back, 0, 64, COL8_FFFFFF, COL8_008484, "10[sec]", 7);
-            }
-            else if (fifo8_status(&timerfifo2) != 0)
-            {
-                i = fifo8_get(&timerfifo2);
-                io_sti();
-                putfonts8_asc_sht(sht_back, 0, 80, COL8_FFFFFF, COL8_008484, "3[sec]", 6);
-            }
-            else if (fifo8_status(&timerfifo3) != 0)
-            {
-                i = fifo8_get(&timerfifo3);
-                io_sti();
-                if (i != 0)
+                if (i == 10)
                 {
-                    timer_init(timer3, &timerfifo3, 0);
-                    boxfill8(buf_back, binfo->scrnx, COL8_FFFFFF, 8, 96, 15, 111);
+                    putfonts8_asc_sht(sht_back, 0, 64, COL8_FFFFFF, COL8_008484, "10[sec]", 7);
+                }
+                else if (i == 3)
+                {
+                    putfonts8_asc_sht(sht_back, 0, 80, COL8_FFFFFF, COL8_008484, "3[sec]", 6);
                 }
                 else
                 {
-                    timer_init(timer3, &timerfifo3, 1);
-                    boxfill8(buf_back, binfo->scrnx, COL8_008484, 8, 96, 15, 111);
+                    if (i != 0)
+                    {
+                        timer_init(timer3, &timerfifo, 0);
+                        boxfill8(buf_back, binfo->scrnx, COL8_FFFFFF, 8, 96, 15, 111);
+                    }
+                    else
+                    {
+                        timer_init(timer3, &timerfifo, 1);
+                        boxfill8(buf_back, binfo->scrnx, COL8_008484, 8, 96, 15, 111);
+                    }
+                    timer_settime(timer3, 50);
+                    sheet_refresh(sht_back, 8, 96, 16, 112);
                 }
-                timer_settime(timer3, 50);
-                sheet_refresh(sht_back, 8, 96, 16, 112);
             }
         }
     }
