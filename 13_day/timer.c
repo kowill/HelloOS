024a0155998df4e28c76824eacd219108f24f892
kowill <mob.koya@gmail.com>
2019-07-22 17:46:02 +0900

    day13 complete

--- 13_day/timer.c
+++ 13_day/timer.c
@@ -5,16 +5,21 @@ struct TIMERCTL timerctl;
 void init_pit(void)
 {
     int i;
+    struct TIMER *t;
     io_out8(PIT_CTRL, 0x34);
     io_out8(PIT_CNT0, 0x9c);
     io_out8(PIT_CNT0, 0x2e);
     timerctl.count = 0;
-    timerctl.next = 0xffffffff;
-    timerctl.using = 0;
     for (i = 0; i < MAX_TIMER; i++)
     {
         timerctl.timers0[i].flags = 0;
     }
+    t = timer_alloc();
+    t->timeout = 0xffffffff;
+    t->flags = TIMER_FLAGS_USING;
+    t->next = 0; // end
+    timerctl.t0 = t;
+    timerctl.next = 0xffffffff;
     return;
 }
 
@@ -53,15 +58,6 @@ void timer_settime(struct TIMER *timer, unsigned int timeout)
     timer->flags = TIMER_FLAGS_USING;
     e = io_load_eflags();
     io_cli();
-    timerctl.using++;
-    if (timerctl.using == 1)
-    {
-        timerctl.t0 = timer;
-        timer->next = 0;
-        timerctl.next = timer->timeout;
-        io_store_eflags(e);
-        return;
-    }
     t = timerctl.t0;
     if (timer->timeout <= t->timeout)
     {
@@ -75,8 +71,6 @@ void timer_settime(struct TIMER *timer, unsigned int timeout)
     {
         s = t;
         t = t->next;
-        if (t == 0)
-            break;
         if (timer->timeout <= t->timeout)
         {
             s->next = timer;
@@ -85,22 +79,17 @@ void timer_settime(struct TIMER *timer, unsigned int timeout)
             return;
         }
     }
-    s->next = timer;
-    timer->next = 0;
-    io_store_eflags(e);
-    return;
 }
 
 void inthandler20(int *esp)
 {
-    int i;
     struct TIMER *timer;
     io_out8(PIC0_OCW2, 0x60);
     timerctl.count++;
     if (timerctl.next > timerctl.count)
         return;
     timer = timerctl.t0;
-    for (i = 0; i < timerctl.using; i++)
+    for (;;)
     {
         if (timer->timeout > timerctl.count)
             break;
@@ -109,12 +98,8 @@ void inthandler20(int *esp)
         fifo32_put(timer->fifo, timer->data);
         timer = timer->next;
     }
-    timerctl.using -= i;
     timerctl.t0 = timer;
+    timerctl.next = timerctl.t0->timeout;
 
-    if (timerctl.using > 0)
-        timerctl.next = timerctl.t0->timeout;
-    else
-        timerctl.next = 0xffffffff;
     return;
 }
