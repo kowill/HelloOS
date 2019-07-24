7c69b0fd1184d0c1517e7d7be06f671e1aa2630e
kowill <mob.koya@gmail.com>
2019-07-24 20:16:06 +0900

    16-1

--- 16_day/bootpack.c
+++ 16_day/bootpack.c
@@ -6,14 +6,6 @@ void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c);
 void putfonts8_asc_sht(struct SHEET *sht, int x, int y, int c, int b, char *s, int l);
 void task_b_main(struct SHEET *sht_back);
 
-struct TSS32
-{
-    int backlink, esp0, ss0, esp1, ss1, esp2, ss2, cr3;
-    int eip, eflags, eax, ecx, edx, ebx, esp, ebp, esi, edi;
-    int es, cs, ss, ds, fs, gs;
-    int ldtr, iomap;
-};
-
 void HariMain(void)
 {
     struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
@@ -27,9 +19,8 @@ void HariMain(void)
     unsigned char *buf_back, buf_mouse[256], *buf_win;
     struct FIFO32 fifo;
     struct TIMER *timer, *timer2, *timer3;
-    int cursor_x, cursor_c, task_b_esp;
-    struct TSS32 tss_a, tss_b;
-    struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *)ADR_GDT;
+    int cursor_x, cursor_c;
+    struct TASK *task_b;
 
     static char keytable[0x54] = {
         0, 0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '^', 0, 0,
@@ -97,34 +88,20 @@ void HariMain(void)
     sprintf(s, "mem: %dMB  free: %dKB", memtotal / (1024 * 1024), memman_total(memman) / 1024);
     putfonts8_asc_sht(sht_back, 0, 48, COL8_FFFFFF, COL8_008484, s, 40);
 
-    tss_a.ldtr = 0;
-    tss_a.iomap = 0x40000000;
-    tss_b.ldtr = 0;
-    tss_b.iomap = 0x40000000;
-    set_segmdesc(gdt + 3, 103, (int)&tss_a, AR_TSS32);
-    set_segmdesc(gdt + 4, 103, (int)&tss_b, AR_TSS32);
-    load_tr(3 * 8);
-    task_b_esp = memman_alloc_4k(memman, 64 * 1024) + 64 * 1024 - 8;
-    tss_b.eip = (int)&task_b_main;
-    tss_b.eflags = 0x00000202; /* IF = 1; */
-    tss_b.eax = 0;
-    tss_b.ecx = 0;
-    tss_b.edx = 0;
-    tss_b.ebx = 0;
-    tss_b.esp = task_b_esp;
-    tss_b.ebp = 0;
-    tss_b.esi = 0;
-    tss_b.edi = 0;
-    tss_b.es = 1 * 8;
-    tss_b.cs = 2 * 8;
-    tss_b.ss = 1 * 8;
-    tss_b.ds = 1 * 8;
-    tss_b.fs = 1 * 8;
-    tss_b.gs = 1 * 8;
+    task_init(memman);
+    task_b = task_alloc();
+    task_b->tss.esp = memman_alloc_4k(memman, 64 * 1024) + 64 * 1024 - 8;
+    task_b->tss.eip = (int)&task_b_main;
+    task_b->tss.es = 1 * 8;
+    task_b->tss.cs = 2 * 8;
+    task_b->tss.ss = 1 * 8;
+    task_b->tss.ds = 1 * 8;
+    task_b->tss.fs = 1 * 8;
+    task_b->tss.gs = 1 * 8;
 
-    *((int *)(task_b_esp + 4)) = (int)sht_back;
+    *((int *)(task_b->tss.esp + 4)) = (int)sht_back;
 
-    mt_init();
+    task_run(task_b);
 
     for (;;)
     {
--- 16_day/timer.c
+++ 16_day/timer.c
@@ -96,7 +96,7 @@ void inthandler20(int *esp)
             break;
 
         timer->flags = TIMER_FLAGS_ALLOC;
-        if (timer != mt_timer)
+        if (timer != task_timer)
         {
             fifo32_put(timer->fifo, timer->data);
         }
@@ -109,6 +109,6 @@ void inthandler20(int *esp)
     timerctl.t0 = timer;
     timerctl.next = timerctl.t0->timeout;
     if (ts != 0)
-        mt_taskswitch();
+        task_switch();
     return;
 }
