adb35bc68060308b4285970bd2c1c15c2cd3020f
kowill <mob.koya@gmail.com>
2019-08-01 16:49:49 +0900

    20-6

--- 20_day/bootpack.h
+++ 20_day/bootpack.h
@@ -44,6 +44,7 @@ unsigned int memtest_sub(unsigned int start, unsigned int end);
 void load_tr(int tr);
 void farjmp(int eip, int cs);
 void farcall(int eip, int cs);
+void asm_cons_putchar(void);
 
 /* graphic.c */
 #define COL8_000000 0
@@ -294,7 +295,7 @@ void cmd_mem(struct CONSOLE *cons, unsigned int memtotal);
 void cmd_cls(struct CONSOLE *cons);
 void cmd_dir(struct CONSOLE *cons);
 void cmd_type(struct CONSOLE *cons, int *fat, char *cmdline);
-void cmd_hlt(struct CONSOLE *cons, int *fat);
+int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline);
 
 /* file.c */
 struct FILEINFO
--- build.ps1
+++ build.ps1
@@ -6,29 +6,30 @@ $targetPath = $targetDay.PadLeft(2, '0') + "_day"
 Remove-Item tmp -recurse -force
 Remove-Item boot.vfd
 New-Item tmp -ItemType Directory
+New-Item tmp\app -ItemType Directory
 
 # nas 2 bin
-@("ipl","asmhead") | %{bin\tolset\z_tools\nask.exe "$($targetPath)\$($_).nas" "tmp\$($_).bin"}
+@("ipl", "asmhead") | % { bin\tolset\z_tools\nask.exe "$($targetPath)\$($_).nas" "tmp\$($_).bin" }
 
-# nas 2 hrb
-@("hlt") | %{bin\tolset\z_tools\nask.exe "$($targetPath)\$($_).nas" "tmp\$($_).hrb"}
+# nas(app) 2 hrb
+Get-ChildItem ($targetPath + '\app') -recurse -include *.nas | % { bin\tolset\z_tools\nask.exe $_.FullName "tmp\app\$([System.IO.Path]::GetFileNameWithoutExtension($_.Name)).hrb" }
 
 # c 2 gas
-Get-ChildItem $targetPath -recurse -include *.c | %{bin\tolset\z_tools\cc1.exe -I bin\tolset\z_tools\haribote\ -Os -Wall -quiet -o "tmp\$([System.IO.Path]::GetFileNameWithoutExtension($_.Name)).gas" $_.FullName}
+Get-ChildItem $targetPath -recurse -include *.c | % { bin\tolset\z_tools\cc1.exe -I bin\tolset\z_tools\haribote\ -Os -Wall -quiet -o "tmp\$([System.IO.Path]::GetFileNameWithoutExtension($_.Name)).gas" $_.FullName }
 
 # gas 2 nas
-Get-ChildItem "tmp\" -recurse -include *.gas | %{bin\tolset\z_tools\gas2nask.exe -a "$($_.FullName)" "tmp\$([System.IO.Path]::GetFileNameWithoutExtension($_.Name)).nas"}
+Get-ChildItem "tmp\" -recurse -include *.gas | % { bin\tolset\z_tools\gas2nask.exe -a "$($_.FullName)" "tmp\$([System.IO.Path]::GetFileNameWithoutExtension($_.Name)).nas" }
 
 # nas 2 obj
-@("naskfunc") | %{bin\tolset\z_tools\nask.exe "$($targetPath)\$($_).nas" "tmp\$($_).obj"}
-Get-ChildItem "tmp\" -recurse -include *.nas | %{bin\tolset\z_tools\nask.exe "$($_.FullName)" "tmp\$([System.IO.Path]::GetFileNameWithoutExtension($_.Name)).obj"}
+@("naskfunc") | % { bin\tolset\z_tools\nask.exe "$($targetPath)\$($_).nas" "tmp\$($_).obj" }
+Get-ChildItem "tmp\" -recurse -include *.nas | % { bin\tolset\z_tools\nask.exe "$($_.FullName)" "tmp\$([System.IO.Path]::GetFileNameWithoutExtension($_.Name)).obj" }
 
 # font
 bin\tolset\z_tools\makefont.exe "$($targetPath)\hankaku.txt" tmp\hankaku.bin
 bin\tolset\z_tools\bin2obj.exe tmp\hankaku.bin tmp\hankaku.obj _hankaku
 
 # obj 2 bim
-$obj = Get-ChildItem "tmp\" -recurse -include *.obj | %{$_.FullName};
+$obj = Get-ChildItem "tmp\" -recurse -include *.obj | % { $_.FullName };
 bin\tolset\z_tools\obj2bim.exe "@bin\tolset\z_tools\haribote\haribote.rul" out:tmp\bootpack.bim stack:3136k map:tmp\bootpack.map $obj
 
 # bim 2 hrb
@@ -37,5 +38,16 @@ bin\tolset\z_tools\bim2hrb.exe tmp\bootpack.bim tmp\bootpack.hrb 0
 # copy
 cmd /c copy /B tmp\asmhead.bin+tmp\bootpack.hrb tmp\haribote.sys
 
+# edimg_script
+$scriptPath = "edimg_script.txt"
+Remove-Item $scriptPath
+Add-Content $scriptPath "imgin:bin\tolset\z_tools\fdimg0at.tek"
+Add-Content $scriptPath "wbinimg src:tmp\ipl.bin len:512 from:0 to:0"
+Add-Content $scriptPath "copy from:tmp\haribote.sys to:@:"
+Add-Content $scriptPath "copy from:readme.md to:@:"
+Add-Content $scriptPath "copy from:build.ps1 to:@:"
+Get-ChildItem "tmp\app" -recurse -include *.hrb | % { Add-Content $scriptPath ("copy from:tmp\app\$($_.Name) to:@:") }
+Add-Content $scriptPath "imgout:boot.vfd"
+
 # edimg
-bin\tolset\z_tools\edimg.exe "@edimg_script.txt"
+bin\tolset\z_tools\edimg.exe "@$($scriptPath)"
