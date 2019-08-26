dbfac0c89dc1c4fd38a766f59f4c6c63b034b3c8
kowill <mob.koya@gmail.com>
2019-08-26 22:44:31 +0900

    init day26 & 26-1

---
+++ 26_day/bootpack.h
@@ -0,0 +1,331 @@
+#define PORT_KEYSTA 0x0064
+#define PORT_KEYCMD 0x0064
+#define EFLAGS_AC_BIT 0x00040000
+#define CR0_CACHE_DISABLE 0x60000000
+#define MEMMAN_FREES 4090
+#define MEMMAN_ADDR 0x003c0000
+struct MEMFREEINFO
+{
+    unsigned int addr, size;
+};
+
+struct MEMMAN
+{
+    int frees, maxfrees, lostsize, losts;
+    struct MEMFREEINFO free[MEMMAN_FREES];
+};
+
+/* asmhead.nas */
+#define ADR_BOOTINFO 0x00000ff0
+#define ADR_DISKING 0x00100000
+
+struct BOOTINFO
+{
+    char cyls, leds, vmode, reserve;
+    short scrnx, scrny;
+    char *vram;
+};
+
+/* naskfunc.nas */
+void io_hlt(void);
+void io_cli(void);
+void io_sti(void);
+void io_stihlt(void);
+int io_in8(int port);
+void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+void asm_inthandler20(void);
+void asm_inthandler21(void);
+void asm_inthandler2c(void);
+void asm_inthandler0d(void);
+void asm_inthandler0c(void);
+int load_cr0(void);
+void store_cr0(int cr0);
+unsigned int memtest_sub(unsigned int start, unsigned int end);
+void load_tr(int tr);
+void farjmp(int eip, int cs);
+void farcall(int eip, int cs);
+void asm_cons_putchar(void);
+void asm_hrb_api(void);
+void start_app(int eip, int cs, int esp, int ds, int *tss_esp0);
+void asm_end_app(void);
+
+/* graphic.c */
+#define COL8_000000 0
+#define COL8_FF0000 1
+#define COL8_00FF00 2
+#define COL8_FFFF00 3
+#define COL8_0000FF 4
+#define COL8_FF00FF 5
+#define COL8_00FFFF 6
+#define COL8_FFFFFF 7
+#define COL8_C6C6C6 8
+#define COL8_840000 9
+#define COL8_008400 10
+#define COL8_848400 11
+#define COL8_000084 12
+#define COL8_840084 13
+#define COL8_008484 14
+#define COL8_848484 15
+
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
+
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
+void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
+void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0, int py0, char *buf, int bxsize);
+
+void show_boxes();
+void init_screen(char *vram, int xsize, int ysize);
+void init_mouse_cursor8(char *mouse, char bc);
+
+/* dsctbl.c */
+#define AR_TSS32 0x0089
+#define AR_INTGATE32 0x008e
+#define AR_DATA32_RW 0x4092
+#define AR_CODE32_ER 0x409a
+#define ADR_GDT 0x00270000
+
+struct SEGMENT_DESCRIPTOR
+{
+    short limit_low, base_low;
+    char base_mid, access_right;
+    char limit_high, base_high;
+};
+
+struct GATE_DESCRIPTOR
+{
+    short offset_low, selector;
+    char dw_count, access_right;
+    short offset_high;
+};
+
+void init_gdtidt(void);
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
+void load_gdtr(int limit, int addr);
+void load_idtr(int limit, int addr);
+
+/* int.c */
+#define PIC0_ICW1 0x0020
+#define PIC0_OCW2 0x0020
+#define PIC0_IMR 0x0021
+#define PIC0_ICW2 0x0021
+#define PIC0_ICW3 0x0021
+#define PIC0_ICW4 0x0021
+#define PIC1_ICW1 0x00a0
+#define PIC1_OCW2 0x00a0
+#define PIC1_IMR 0x00a1
+#define PIC1_ICW2 0x00a1
+#define PIC1_ICW3 0x00a1
+#define PIC1_ICW4 0x00a1
+#define PORT_KEYDAT 0x0060
+
+void init_pic(void);
+
+/* fifl.c */
+struct FIFO32
+{
+    int *buf;
+    int p, q, size, free, flags;
+    struct TASK *task;
+};
+
+void fifo32_init(struct FIFO32 *fifo, int size, int *buf, struct TASK *task);
+int fifo32_put(struct FIFO32 *fifo, int data);
+int fifo32_get(struct FIFO32 *fifo);
+int fifo32_status(struct FIFO32 *fifo);
+
+/* keyboard.c */
+#define PORT_KEYDAT 0x0060
+#define KEYSTA_SEND_NORTEADY 0x02
+#define KEYCMD_WRITE_MODE 0x60
+#define KBC_MODE 0x47
+
+void inthandler21(int *esp);
+void init_keyboard(struct FIFO32 *fifo, int data0);
+void wait_KBC_sendready(void);
+
+/* mouse.c */
+#define KEYCMD_SENDTO_MOUSE 0xd4
+#define MOUSECMD_ENABLE 0xf4
+struct MOUSE_DEC
+{
+    unsigned char buf[3], phase;
+    int x, y, btn;
+};
+
+void inthandler2c(int *esp);
+void enable_mouse(struct FIFO32 *fifo, int data0, struct MOUSE_DEC *mdec);
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char data);
+
+/* memory.c */
+unsigned int memtest(unsigned int start, unsigned int end);
+void memman_init(struct MEMMAN *man);
+unsigned int memman_total(struct MEMMAN *man);
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
+unsigned int memman_alloc_4k(struct MEMMAN *man, unsigned int size);
+int memman_free_4k(struct MEMMAN *man, unsigned int addr, unsigned int size);
+
+/* sheet.c */
+#define MAX_SHEETS 256
+#define SHEET_USE 1
+
+struct SHEET
+{
+    unsigned char *buf;
+    int bxsize, bysize, vx0, vy0, col_inv, height, flags;
+    struct SHTCTL *ctl;
+    struct TASK *task;
+};
+
+struct SHTCTL
+{
+    unsigned char *vram, *map;
+    int xsize, ysize, top;
+    struct SHEET *sheets[MAX_SHEETS];
+    struct SHEET sheetsO[MAX_SHEETS];
+};
+
+struct SHTCTL *shtctl_init(struct MEMMAN *memman, unsigned char *vram, int xsize, int ysize);
+struct SHEET *sheet_alloc(struct SHTCTL *ctl);
+void sheet_setbuf(struct SHEET *sht, unsigned char *buf, int xsize, int ysize, int col_inv);
+void sheet_updown(struct SHEET *sht, int height);
+void sheet_refresh(struct SHEET *sht, int bx0, int by0, int bx1, int by1);
+void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, int h0, int h1);
+void sheet_refreshmap(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, int h0);
+void sheet_slide(struct SHEET *sht, int vx0, int vy0);
+void sheet_free(struct SHEET *sht);
+
+/* timer.c */
+#define PIT_CTRL 0x0043
+#define PIT_CNT0 0x0040
+#define MAX_TIMER 500
+#define TIMER_FLAGS_ALLOC 1
+#define TIMER_FLAGS_USING 2
+
+struct TIMER
+{
+    struct TIMER *next;
+    unsigned int timeout;
+    char flags, flags2;
+    struct FIFO32 *fifo;
+    int data;
+};
+
+struct TIMERCTL
+{
+    unsigned int count, next;
+    struct TIMER *t0;
+    struct TIMER timers0[MAX_TIMER];
+};
+
+extern struct TIMERCTL timerctl;
+void init_pit(void);
+void inthandler20(int *esp);
+struct TIMER *timer_alloc(void);
+void timer_init(struct TIMER *timer, struct FIFO32 *fifo, int data);
+void timer_settime(struct TIMER *timer, unsigned int timeout);
+void timer_free(struct TIMER *timer);
+int timer_cancel(struct TIMER *timer);
+void timer_cancelall(struct FIFO32 *fifo);
+
+/* mtask.c */
+#define MAX_TASKS 1000
+#define MAX_TASKS_LV 100
+#define MAX_TASKLEVELS 10
+#define TASK_GDT0 3
+#define TASK_FLG_NOUSE 0
+#define TASK_FLG_USING 1
+#define TASK_FLG_RUNNING 2
+
+struct TSS32
+{
+    int backlink, esp0, ss0, esp1, ss1, esp2, ss2, cr3;
+    int eip, eflags, eax, ecx, edx, ebx, esp, ebp, esi, edi;
+    int es, cs, ss, ds, fs, gs;
+    int ldtr, iomap;
+};
+
+struct TASK
+{
+    int sel, flags; // sel -> gdt number
+    int level, priority;
+    struct FIFO32 fifo;
+    struct TSS32 tss;
+    struct CONSOLE *cons;
+    int ds_base;
+};
+
+struct TASKLEVEL
+{
+    int running;
+    int now;
+    struct TASK *tasks[MAX_TASKS_LV];
+};
+
+struct TASKCTL
+{
+    int now_lv;
+    char lv_change;
+    struct TASKLEVEL level[MAX_TASKLEVELS];
+    struct TASK tasks0[MAX_TASKS];
+};
+
+extern struct TIMER *task_timer;
+struct TASK *task_init(struct MEMMAN *memman);
+struct TASK *task_alloc(void);
+void task_run(struct TASK *task, int level, int priority);
+void task_switch(void);
+void task_sleep(struct TASK *task);
+struct TASK *task_now(void);
+void task_add(struct TASK *task);
+void task_remove(struct TASK *task);
+void task_switchsub(void);
+void task_idol(void);
+
+/* window.c */
+void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char act);
+void make_wtitle8(unsigned char *buf, int xsize, char *title, char act);
+void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c);
+void putfonts8_asc_sht(struct SHEET *sht, int x, int y, int c, int b, char *s, int l);
+void change_wtitle8(struct SHEET *sht, char act);
+
+/* console.c */
+struct CONSOLE
+{
+    struct SHEET *sht;
+    int cur_x, cur_y, cur_c;
+    struct TIMER *timer;
+};
+
+void console_task(struct SHEET *sheet, unsigned int memtotal);
+void cons_putchar(struct CONSOLE *cons, int chr, char move);
+void cons_newline(struct CONSOLE *cons);
+void cons_runcmd(char *cmdline, struct CONSOLE *cons, int *fat, unsigned int memtotal);
+void cons_putstr0(struct CONSOLE *cons, char *s);
+void cons_putstr1(struct CONSOLE *cons, char *s, int l);
+void cmd_mem(struct CONSOLE *cons, unsigned int memtotal);
+void cmd_cls(struct CONSOLE *cons);
+void cmd_dir(struct CONSOLE *cons);
+void cmd_type(struct CONSOLE *cons, int *fat, char *cmdline);
+int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline);
+int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int eax);
+int *inthandler0d(int *esp);
+int *inthandler0c(int *esp);
+void hrb_api_linewin(struct SHEET *sht, int x0, int y0, int x1, int y1, int col);
+
+/* file.c */
+struct FILEINFO
+{
+    unsigned char name[8], ext[3], type;
+    char reserve[10];
+    unsigned short time, date, clustno;
+    unsigned int size;
+};
+void file_readfat(int *fat, unsigned char *img);
+void file_loadfile(int clustno, int size, char *buf, int *fat, char *img);
+struct FILEINFO *file_search(char *name, struct FILEINFO *finfo, int max);
---
+++ 26_day/ipl.nas
@@ -0,0 +1,108 @@
+; hello-os
+; TAB=4
+
+CYLS    EQU 10
+
+    ORG 0x7c00
+
+; For FAT12 Format FloppyDisk
+
+    JMP entry
+    DB  0x90
+    DB  "BOOTNAME"
+    DW  512
+    DB  1
+    DW  1
+    DB  2
+    DW  224
+    DW  2880
+    DB  0xf0
+    DW  9
+    DW  18
+    DW  2
+    DD  0
+    DD  2880
+    DB  0, 0, 0x29
+    DD  0xffffffff
+    DB  "HELLO-OS   "
+    DB  "FAT12   "
+    RESB    18
+
+; 本体
+
+entry:
+    MOV AX, 0
+    MOV SS, AX
+    MOV SP, 0x7c00
+    MOV DS, AX
+    
+; read disk
+	MOV AX, 0x0820
+    MOV ES, AX
+    MOV CH, 0       ; シリンダー0
+    MOV DH, 0       ; Head 0
+    MOV CL, 2       ; Sector2
+
+readloop:
+    MOV SI, 0       ; 失敗回数用
+
+retry:
+    MOV AH, 0x02    ; read disk
+    MOV AL, 1
+    MOV BX, 0
+    MOV DL, 0x00
+    INT 0x13
+    JNC next
+    ADD SI, 1
+    CMP SI, 5
+    JAE error
+    MOV AH, 0x00
+    MOV DL, 0x00
+    INT 0x13        ; reset drive
+    JMP retry
+next:
+    MOV AX,ES
+    ADD AX, 0x0020  ; 512/16
+    MOV ES, AX
+    ADD CL, 1
+    CMP CL, 18
+    JBE readloop
+    MOV CL, 1
+    ADD DH, 1
+    CMP DH, 2
+    JB  readloop
+    MOV DH, 0
+    ADD CH, 1
+    CMP CH, CYLS
+    JB  readloop
+
+; exexute haribote.sys 
+    MOV [0x0ff0], CH    ; IPLが読み込んだCYLS数をメモ
+    JMP 0xc200
+
+fin:
+    HLT
+    JMP fin
+
+error:
+    MOV SI, msg
+
+putloop:
+    MOV AL, [SI]
+    ADD SI, 1
+    CMP AL, 0
+    JE  fin
+    MOV AH, 0x0e
+    MOV BX, 15
+    INT 0x10
+    JMP putloop
+
+msg:
+    DB  0x0a, 0x0a
+    DB  "load error"
+    DB  0x0a
+    DB  0
+
+    RESB    0x7dfe-$
+
+    DB  0x55, 0xaa
