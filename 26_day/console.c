ab2188cc5c6c015beefa02965c03202c86b4d055
kowill <mob.koya@gmail.com>
2019-09-03 00:33:24 +0900

    26-9

--- 26_day/console.c
+++ 26_day/console.c
@@ -172,6 +172,8 @@ void cons_runcmd(char *cmdline, struct CONSOLE *cons, int *fat, unsigned int mem
         cmd_type(cons, fat, cmdline);
     else if (strcmp(cmdline, "exit") == 0)
         cmd_exit(cons, fat);
+    else if (strncmp(cmdline, "start ", 6) == 0)
+        cmd_start(cons, cmdline, memtotal);
     else if (cmdline[0] != 0)
     {
         if (cmd_app(cons, fat, cmdline) == 0)
@@ -556,4 +558,19 @@ void cmd_exit(struct CONSOLE *cons, int *fat)
     io_sti();
     for (;;)
         task_sleep(task);
+}
+
+void cmd_start(struct CONSOLE *cons, char *cmdline, int memtotal)
+{
+    struct SHTCTL *shtctl = (struct SHTCTL *)*((int *)0x0fe4);
+    struct SHEET *sht = open_console(shtctl, memtotal);
+    struct FIFO32 *fifo = &sht->task->fifo;
+    int i;
+    sheet_slide(sht, 32, 4);
+    sheet_updown(sht, shtctl->top);
+    for (i = 6; cmdline[i] != 0; i++)
+        fifo32_put(fifo, cmdline[i] + 256);
+    fifo32_put(fifo, 10 + 256);
+    cons_newline(cons);
+    return;
 }
